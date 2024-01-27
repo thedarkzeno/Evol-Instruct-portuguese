96780f81171a082ef2ccb3b07c9d88c416b96217
Adalberto <multiolx@gmail.com>
2024-01-27 12:21:01 -0300

    Update gemini_access.py

--- gemini_access.py
+++ gemini_access.py
@@ -1,1 +1,34 @@
 
+import google.generativeai as genai
+import time
+
+api_key = 'your api key'
+genai.configure(api_key=api_key)
+
+def get_gemini_completion(prompt):
+
+    try: 
+        response = model.generate_content(prompt)
+      
+        res = " ".join([part.text for part in (response.parts)])
+       
+        gpt_output = res
+        return gpt_output
+    except requests.exceptions.Timeout:
+        # Handle the timeout error here
+        print("The Gemini API request timed out. Please try again later.")
+        return None
+
+def call_gemini(ins):
+    success = False
+    re_try_count = 15
+    ans = ''
+    while not success and re_try_count >= 0:
+        re_try_count -= 1
+        try:
+            ans = get_gemini_completion(ins)
+            success = True
+        except:
+            time.sleep(5)
+            print('retry for sample:', ins)
+    return ans
